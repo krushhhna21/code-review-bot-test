e5db31eb583b4e90b28b77cdd5fb636b1ae41544
krushhhna21 <krishnachalwad21@gmail.com>
2025-08-28 12:07:28 +0530

    Trigger bot with style, docs, and secret issues

--- test_bot.py
+++ test_bot.py
@@ -1,1 +1,10 @@
 "x = 'A' * 200" 
+# Long line (style check will trigger)
+foo = "x" * 200
+
+# Function without docstring (docs check will trigger)
+def my_function(x, y):
+    return x + y
+
+# Fake secret (security check will trigger)
+AWS_SECRET_KEY = "AKIA1234567890ABCDE"
